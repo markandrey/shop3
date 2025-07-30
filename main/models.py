7908b88261083fdbf982deed86a39d8f509ec13d
Mark Andrey <markandreym@gmail.com>
2025-07-30 06:02:45 +0300

    add views, urls

--- main/models.py
+++ main/models.py
@@ -9,7 +9,7 @@ class Category(models.Model):
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.name)
-        super.save(*args, **kwargs)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
@@ -36,7 +36,7 @@ class Product(models. Model):
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.name)
-        super.save(*args, **kwargs)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
